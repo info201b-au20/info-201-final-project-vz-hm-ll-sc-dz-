12fb7f8ab55ddc73dc17a0bbc18afe0ec3b3ded7
Vivien Zhang <viv@Viviens-MacBook-Pro-2.local>
2020-11-11 01:06:30 -0800

    chart 3 pf ef scatterplot

---
+++ scripts/chart_3_scatterplot.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(RColorBrewer)
+
+hfi_2019_data <- read.csv("../hfi_cc_2019.csv")
+
+#pf = personal freedom
+pf_data <- hfi_2019_data %>%
+  select(year, countries, region, pf_score)
+
+pf_data_by_region <- pf_data %>%
+  arrange(-year, region)
+  
+ef_data <- hfi_2019_data %>%
+  select(year, countries, region, ef_score)
+
+ef_data_by_region <- ef_data %>%
+  arrange(-year, region)
+
+total_data <- pf_data_by_region %>%
+  mutate(ef_score = ef_data_by_region$ef_score)
+
+total_data_2017 <- total_data %>%
+  filter(year == max(year))
+
+ggplot(data = total_data_2017) +
+  geom_point(
+    mapping = aes(x = pf_score, y = ef_score, color = region)
+    ) +
+    scale_color_brewer(palette = "Set3") +
+    labs(
+      title = paste("Personal and Economic Freedom 2017"),
+      x = "Personal Freedom",
+      y = "Economic Freedom")
+
+#ggplot(data = total_data) +
+#  geom_point(
+#    mapping = aes(x = pf_score, y = ef_score, color = region)
+#  ) +
+#  scale_color_brewer(palette = "Set3") + facet_wrap(~year) +
+#  labs(
+#    title = paste("Personal and Economic Freedom"),
+#    x = "Personal Freedom",
+#    y = "Economic Freedom")
